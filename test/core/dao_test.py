fb8878e0b21d9d0185f848fa2345d90fc5d4ee03
Kalimaha <guido.barbaglia@gmail.com>
2015-06-27 18:40:59 +0200

    Setup test.

--- test/core/dao_test.py
+++ test/core/dao_test.py
@@ -5,11 +5,15 @@ from a_la_romana_services.core.dao import DAO
 class DAOTestCase(unittest.TestCase):
 
     dao = None
+    db_name = "a_la_romana_test_db"
+    users = "test_users"
+    activities = "test_activities"
+    events = "test_events"
     config = {
-        "db_name": "a_la_romana_test_db",
-        "users": "test_users",
-        "activities": "test_activities",
-        "events": "test_events"
+        "db_name": db_name,
+        "users": users,
+        "activities": activities,
+        "events": events
     }
 
     def setUp(self):
@@ -17,3 +21,7 @@ class DAOTestCase(unittest.TestCase):
 
     def test_setup(self):
         self.assertIsNotNone(self.dao)
+        self.assertEquals(self.dao.db_name, self.db_name)
+        self.assertEquals(self.dao.users, self.users)
+        self.assertEquals(self.dao.activities, self.activities)
+        self.assertEquals(self.dao.events, self.events)
