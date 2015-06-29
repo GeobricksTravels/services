623984e9876cdaeaea500f0e9376c14fd19e1d06
Kalimaha <guido.barbaglia@gmail.com>
2015-06-29 15:00:12 +0200

    print

--- a_la_romana_services/start.py
+++ a_la_romana_services/start.py
@@ -3,7 +3,9 @@ from flask.ext.cors import CORS
 from a_la_romana_services.rest.dao_rest import dao_rest
 
 
+print 'init flask...'
 app = Flask(__name__)                                   # pragma: no cover
+print 'init cors...'
 cors = CORS(app,                                        # pragma: no cover
             resources={                                 # pragma: no cover
                 r'/*': {                                # pragma: no cover
@@ -11,11 +13,8 @@ cors = CORS(app,                                        # pragma: no cover
                     'headers': ['Content-Type']         # pragma: no cover
                 }                                       # pragma: no cover
             })                                          # pragma: no cover
+print 'register blueprint...'
 app.register_blueprint(dao_rest, url_prefix='/dao')     # pragma: no cover
-
-@app.route('/', methods=['GET'])
-def say_hello():
-    return 'hello?'
-
+print 'app run'
 if __name__ == '__main__':                              # pragma: no cover
     app.run(debug=True, threaded=True)                  # pragma: no cover
